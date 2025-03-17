655656d3ecb444c02831bb97ed55a3094e20d987
kallumaguy <adilahmed.aa786@gmail.com>
2025-03-17 09:07:02 +0530

    updated overflow

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-  AOS.init(); // Initialize AOS
+  AOS.init({
+    offset: 25, // Offset (in px) from the original trigger point
+    delay: 0, // Delay time (in ms) before the animation starts
+    duration: 500, // Duration of the animation (in ms)
+    easing: "ease", // Easing function for the animation
+    once: true, // Run animation once (true) or every time (false)
+  });
 });
 
-AOS.init({
-  offset: 25, // Offset (in px) from the original trigger point
-  delay: 0, // Delay time (in ms) before the animation starts
-  duration: 500, // Duration of the animation (in ms)
-  easing: "ease", // Easing function for the animation
-  once: true, // Run animation once (true) or every time (false)
-});
